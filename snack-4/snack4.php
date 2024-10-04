2a8a473195b5b46bd4d6843f16a9c30a63c1ec1d
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-04 16:20:54 +0200

    got the class list file

--- snack-4/snack4.php
+++ snack-4/snack4.php
@@ -1,5 +1,9 @@
 <!-- Snack 4A:
 Stampare in pagina, senza particolare stilizzazione il nome di ogni classe e sotto ognuna, i dati di ogni studente/studentessa presente nella relativa classe. -->
+<?php
+// vado a prendere la lista delle classi
+require_once __DIR__ . '/list_classes.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
