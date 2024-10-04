6a3d8addd398ceab112103cf6a70bfd97091188f
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-04 16:08:09 +0200

    created a new folder for snack 4 and created a new file for list of classes

---
+++ snack-4/snack4.php
@@ -0,0 +1,17 @@
+<!-- Snack 4A:
+Stampare in pagina, senza particolare stilizzazione il nome di ogni classe e sotto ognuna, i dati di ogni studente/studentessa presente nella relativa classe. -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack-4</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
