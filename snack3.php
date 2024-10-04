bf3e914a0dba74a2cf1e1cf9c92e5cafb8e08b6f
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-04 12:49:38 +0200

    added teachers in page

---
+++ snack3.php
@@ -0,0 +1,63 @@
+<!-- Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. -->
+
+<?php
+
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack 3</title>
+</head>
+
+<body>
+    <h2>Teachears</h2>
+    <ul class="gray">
+        <!-- giro nella lista degli insegnanti e stampo in pagina il nome e cognome di ogni teacher -->
+        <?php foreach ($db['teachers'] as $teacher) { ?>
+        <li> <?php echo $teacher['name'] . ' ' . $teacher['lastname'] ?></li>
+        <?php } ?>
+    </ul>
+    <h2>
+        PM
+    </h2>
+    <ul class="g">
+        <li>
+
+        </li>
+    </ul>
+
+</body>
+
+</html>
+<style>
+.gray {
+    background-color: gray;
+}
+</style>
