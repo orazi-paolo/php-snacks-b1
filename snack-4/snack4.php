a102a08dc86842ba691752f093b1078239e55a46
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-04 16:52:48 +0200

    added filter for students with average grade higher than 6

--- snack-4/snack4.php
+++ snack-4/snack4.php
@@ -1,8 +1,9 @@
-<!-- Snack 4A:
-Stampare in pagina, senza particolare stilizzazione il nome di ogni classe e sotto ognuna, i dati di ogni studente/studentessa presente nella relativa classe. -->
+<!-- Snack 4B:
+Filtrare il nostro array e mostrare, sempre suddivisi per classe, esclusivamente gli studenti e le studentesse con voto medio sufficiente. -->
 <?php
 // vado a prendere la lista delle classi
 require_once __DIR__ . '/list_classes.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -15,24 +16,27 @@ require_once __DIR__ . '/list_classes.php';
 </head>
 
 <body>
+    <!-- ciclo sulle classi per prendere l array dello studente -->
     <?php foreach ($classi as $classe => $studente) { ?>
     <ul>
         <li>
             <?php echo $classe; ?>
         </li>
         <?php
+            // ciclo sull array dello studente per prendere i suoi valori come nome ecc...
             foreach ($studente as $description) {
+                // filtro tra gli studenti con voto medio maggiore di 6
+                if ($description['voto_medio'] > 6) {
             ?>
         <li>
             <?php
-                    echo $description['nome'] . ' ' . $description['cognome'] . ', ' . $description['anni'] . ' anni, ' . 'voto medio: ' . $description['voto_medio'] . ', linguaggio preferito: ' . $description['linguaggio_preferito'];
-                    ?>
-        </li>
-        <li>
+                        echo $description['nome'] . ' ' . $description['cognome'] . ', ' . $description['anni'] . ' anni, ' . 'voto medio: ' . $description['voto_medio'] . ', linguaggio preferito: ' . $description['linguaggio_preferito'];
+                        ?>
             <img src="<?php echo $description['immagine']; ?>"
                 alt="<?php echo $description['nome'] . ' ' . $description['cognome']; ?>">
         </li>
         <?php
+                }
             }
             ?>
     </ul>
